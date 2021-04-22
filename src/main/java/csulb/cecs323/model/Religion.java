92edceb82cfab424481bec1df4cd91277f84809c
John Quach <quachjohnhien@gmail.com>
2021-04-21 23:32:50 -0700

    Documentation
    For region, religion, type, and user

--- src/main/java/csulb/cecs323/model/Religion.java
+++ src/main/java/csulb/cecs323/model/Religion.java
@@ -12,9 +12,11 @@ import java.util.Set;
 @Table(name = "RELIGIONS")
 public class Religion
 {
+    /** The name of a cuisine that pertains to a religion. */
     @Id
     private String name;
 
+    /** The list of cuisines that pertains to a religion. */
     @OneToMany(mappedBy = "religion")
     private
     Set<Cuisine> cuisines;
@@ -39,6 +41,11 @@ public class Religion
         return cuisines;
     }
 
+
+    /**
+     * Adds an cuisine to a set of cuisines
+     * @param cuisine   An cuisine object that pertains to a certain religion
+     */
     public void addCuisine(Cuisine cuisine) {
         boolean added =  this.cuisines.add(cuisine);
         if (added)
